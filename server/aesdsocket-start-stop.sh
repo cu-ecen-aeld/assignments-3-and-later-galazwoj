c4483b11f4f649dd784aa60870dc5f41227d8ceb
Wojciech Galazka <wojciech.galazka@gmail.com>
2023-11-12 12:04:29 +0100

    fixed daemon script

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -2,21 +2,58 @@
 
 DAEMON="aesdsocket"
 
+start() {
+	printf 'Starting %s: ' "$DAEMON"
+	start-stop-daemon -S -x "/usr/bin/$DAEMON" -- -d
+	status=$?
+	if [ "$status" -eq 0 ]; then
+		echo "OK"
+	else
+		echo "FAIL"
+	fi
+	return "$status"
+}
+
+stop() {
+	printf 'Stopping %s: ' "$DAEMON"
+	start-stop-daemon -K -n $DAEMON
+	status=$?
+	if [ "$status" -eq 0 ]; then
+		echo "OK"
+	else
+		echo "FAIL"
+	fi
+	return "$status"
+}
+
+restart() {
+	stop
+	sleep 1
+	start
+}
+
+status() {
+	printf 'Status %s: ' "$DAEMON"
+	start-stop-daemon -T -n $DAEMON
+	status=$?
+	if [ "$status" -eq 0 ]; then
+		echo "OK"
+	else
+		echo "FAILED"
+	fi
+	return "$status"
+
+}
+
 case "$1" in
-	start)
-		echo "Starting $DAEMON"
-		start-stop-daemon -S -n $DAEMON -a /usr/bin/$DAEMON -- -d
+	start|stop|restart|status)
+		"$1"
 		;;
-	stop)
-		echo "Stopping $DAEMON"
-		start-stop-daemon -K -n $DAEMON
-		;;
-	restart)
-		"$0" stop
-		"$0" start
+	reload)
+		# Restart, since there is no true "reload" feature.
+		restart
 		;;
 	*)
-		echo "Usage: $0 {start|stop|restart}"
+		echo "Usage: $0 {start|stop|restart|reload|status}"
 		exit 1
-		;;
 esac
